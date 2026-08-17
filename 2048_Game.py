58c7b0d09d2f3b09c2bd16fea84613db2d213903
seif desktop <seif.t.elkady@gmail.com>
2026-08-17 16:04:46 -0400

    8_17_2026(Finally fixed bug and and made lose game screen. I am going to make homescreen next)

--- 2048_Game.py
+++ 2048_Game.py
@@ -172,47 +172,43 @@ class Game(QMainWindow):
         self.spawn_square()
 
     def spawn_square(self):
-        try:
-            self.box_number= random.randint(0, 15)
+        self.box_number= random.randint(0, 15)
+
+        if not self.game_list[self.box_number] == 0:
+            self.spawn_square()
+            return
+
+        add_box=self.boxes[self.box_number]
+
+        numbers= ['2', '4', '8']
+        weights= [50, 40, 10]
+
+        number= random.choices(numbers, weights=weights)[0]
+
+        if number == '2':
+            add_box.setText('2')
+            add_box.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            add_box.setStyleSheet(self.two)
+            self.game_list[self.box_number] = 2 # type: ignore
+
+        elif number == '4':
+            add_box.setText('4')
+            add_box.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            add_box.setStyleSheet(self.four)
+            self.game_list[self.box_number] = 4 # type: ignore
+
+        elif number == '8':
+            add_box.setText('8')
+            add_box.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            add_box.setStyleSheet(self.eight)
+            self.game_list[self.box_number] = 8 # type: ignore
 
 
-            if not self.game_list[self.box_number] == 0:
-                self.spawn_square()
-                return
+        for i in range(0, len(self.game_list), 4):
+            print(*self.game_list[i : i + 4])
 
-            add_box=self.boxes[self.box_number]
+        self.game_lines()
 
-            numbers= ['2', '4', '8']
-            weights= [50, 40, 10]
-
-            number= random.choices(numbers, weights=weights)[0]
-
-            if number == '2':
-                add_box.setText('2')
-                add_box.setAlignment(Qt.AlignmentFlag.AlignCenter)
-                add_box.setStyleSheet(self.two)
-                self.game_list[self.box_number] = 2 # type: ignore
-
-            elif number == '4':
-                add_box.setText('4')
-                add_box.setAlignment(Qt.AlignmentFlag.AlignCenter)
-                add_box.setStyleSheet(self.four)
-                self.game_list[self.box_number] = 4 # type: ignore
-
-            elif number == '8':
-                add_box.setText('8')
-                add_box.setAlignment(Qt.AlignmentFlag.AlignCenter)
-                add_box.setStyleSheet(self.eight)
-                self.game_list[self.box_number] = 8 # type: ignore
-
-
-            for i in range(0, len(self.game_list), 4):
-                print(*self.game_list[i : i + 4])
-
-            self.game_lines()
-
-        except RecursionError:
-            self.lose_game()
 
     #The name of the function matters in this case
     def keyPressEvent(self, event):
@@ -244,38 +240,38 @@ class Game(QMainWindow):
             self.move_down()
 
     def move_left(self):
-        for n in range(len(self.rows)):
-            row= self.rows[n]
-            #You need range(len(nums)). You can just do in nums
-            x=0
-            for i in range(len(row)):
-                if row[i]:
-                    #Use line below to switch values
-                    row[x], row[i] = row[i], row[x]
-                    x+=1
-                    
-            #To understand it get a sheet of paper and write it out
-            for j in range(len(row)):
-                if row[j]:
-                    try:
+        try:
+            for n in range(len(self.rows)):
+                row= self.rows[n]
+                #You need range(len(nums)). You can just do in nums
+                x=0
+                for i in range(len(row)):
+                    if row[i]:
+                        #Use line below to switch values
+                        row[x], row[i] = row[i], row[x]
+                        x+=1
+                        
+                #To understand it get a sheet of paper and write it out
+                for j in range(len(row)):
+                    if row[j] and j > 0:
                         if row[j] == row[j-1]:
                             row[j-1]= row[j] + row[j-1]
                             row[j]=0
 
-                    except IndexError:
-                        pass
+                y=0
+                for i in range(len(row)):
+                    if row[i]:
+                        #Use line below to switch values
+                        row[y], row[i] = row[i], row[y]
+                        y+=1
+                
 
+            x=0
             y=0
-            for i in range(len(row)):
-                if row[i]:
-                    #Use line below to switch values
-                    row[y], row[i] = row[i], row[y]
-                    y+=1
-            
-
-        x=0
-        y=0
-        self.update_game_rows()
+            self.update_game_rows()
+        
+        except RecursionError:
+            self.lose_game()
 
         print('\n')
         for i in range(0, len(self.game_list), 4):
@@ -283,39 +279,39 @@ class Game(QMainWindow):
 
 
     def move_right(self):
-        for n in range(len(self.rows)):
-            row= self.rows[n]
-            x=len(row) -1
+        try:
+            for n in range(len(self.rows)):
+                row= self.rows[n]
+                x=len(row) -1
 
-            for i in range(len(row)-1, -1, -1):
-                if row[i]:
-                    #Use line below to switch values
-                    row[x], row[i] = row[i], row[x]
-                    x-=1
+                for i in range(len(row)-1, -1, -1):
+                    if row[i]:
+                        #Use line below to switch values
+                        row[x], row[i] = row[i], row[x]
+                        x-=1
 
-            #To figure out write it out
-            for j in range(len(row)-1, -1, -1):
-                if row[j]:
-                    try:
+                #To figure out write it out
+                for j in range(len(row)-1, -1, -1):
+                    if row[j] and j+1 < len(row):
                         if row[j] == row[j+1]:
                             row[j]= row[j] + row[j+1]
                             row[j+1]=0
 
-                    except IndexError:
-                        pass
+
+                y=len(row) -1
+                for i in range(len(row)-1, -1, -1):
+                    if row[i]:
+                        #Use line below to switch values
+                        row[y], row[i] = row[i], row[y]
+                        y-=1
 
 
-            y=len(row) -1
-            for i in range(len(row)-1, -1, -1):
-                if row[i]:
-                    #Use line below to switch values
-                    row[y], row[i] = row[i], row[y]
-                    y-=1
-
-
-        x=0
-        y=0
-        self.update_game_rows()
+            x=0
+            y=0
+            self.update_game_rows()
+        
+        except RecursionError:
+            self.lose_game()
 
         print('\n')
         for i in range(0, len(self.game_list), 4):
@@ -323,77 +319,80 @@ class Game(QMainWindow):
 
 
     def move_up(self):
-        for n in range(len(self.colmuns)):
-            colmun= self.colmuns[n]
-            #You need range(len(nums)). You can just do in nums
+        try:
+            for n in range(len(self.colmuns)):
+                colmun= self.colmuns[n]
+                #You need range(len(nums)). You can just do in nums
+                x=0
+                for i in range(len(colmun)):
+                    if colmun[i]:
+                        #Use line below to switch values
+                        colmun[x], colmun[i] = colmun[i], colmun[x]
+                        x+=1
+                        
+
+                for j in range(len(colmun)):
+                    if colmun[j] and j > 0:
+                            if colmun[j] == colmun[j-1]:
+                                colmun[j-1]= colmun[j] + colmun[j-1]
+                                colmun[j]=0
+
+
+                y=0
+                for i in range(len(colmun)):
+                    if colmun[i]:
+                        #Use line below to switch values
+                        colmun[y], colmun[i] = colmun[i], colmun[y]
+                        y+=1
+                
+
             x=0
-            for i in range(len(colmun)):
-                if colmun[i]:
-                    #Use line below to switch values
-                    colmun[x], colmun[i] = colmun[i], colmun[x]
-                    x+=1
-                    
-
-            for j in range(len(colmun)):
-                if colmun[j]:
-                    try:
-                        if colmun[j] == colmun[j-1]:
-                            colmun[j-1]= colmun[j] + colmun[j-1]
-                            colmun[j]=0
-
-                    except IndexError:
-                        pass
-
-
-
             y=0
-            for i in range(len(colmun)):
-                if colmun[i]:
-                    #Use line below to switch values
-                    colmun[y], colmun[i] = colmun[i], colmun[y]
-                    y+=1
-            
+            self.update_game_colmuns()
 
-        x=0
-        y=0
-        self.update_game_colmuns()
+        except RecursionError:
+            self.lose_game()
 
         print('\n')
         for i in range(0, len(self.game_list), 4):
             print(*self.game_list[i : i + 4])
 
     def move_down(self):
-        for n in range(len(self.colmuns)):
-            colmun= self.colmuns[n]
-            x=len(colmun) -1
+        try:
+            for n in range(len(self.colmuns)):
+                colmun= self.colmuns[n]
+                x=len(colmun) -1
 
-            for i in range(len(colmun)-1, -1, -1):
-                if colmun[i]:
-                    #Use line below to switch values
-                    colmun[x], colmun[i] = colmun[i], colmun[x]
-                    x-=1
+                for i in range(len(colmun)-1, -1, -1):
+                    if colmun[i]:
+                        #Use line below to switch values
+                        colmun[x], colmun[i] = colmun[i], colmun[x]
+                        x-=1
 
-            for j in range(len(colmun)-1, -1, -1):
-                if colmun[j]:
-                    try:
-                        if colmun[j] == colmun[j+1]:
-                            colmun[j+1]= colmun[j] + colmun[j+1]
-                            colmun[j]=0
+                for j in range(len(colmun)-1, -1, -1):
+                    if colmun[j] and j+1 < len(colmun):
+                        try:
+                            if colmun[j] == colmun[j+1]:
+                                colmun[j+1]= colmun[j] + colmun[j+1]
+                                colmun[j]=0
 
-                    except IndexError:
-                        pass
+                        except IndexError:
+                            pass
 
-            y=len(colmun) -1
-            for i in range(len(colmun)-1, -1, -1):
-                if colmun[i]:
-                    #Use line below to switch values
-                    colmun[y], colmun[i] = colmun[i], colmun[y]
-                    y-=1
+                y=len(colmun) -1
+                for i in range(len(colmun)-1, -1, -1):
+                    if colmun[i]:
+                        #Use line below to switch values
+                        colmun[y], colmun[i] = colmun[i], colmun[y]
+                        y-=1
 
 
-        x=0
-        y=0
-        self.update_game_colmuns()
+            x=0
+            y=0
+            self.update_game_colmuns()
+
+        except RecursionError:
+            self.lose_game()
 
         print('\n')
         for i in range(0, len(self.game_list), 4):
@@ -568,9 +567,20 @@ class Game(QMainWindow):
         self.colmuns_2= self.colmuns
 
         if x == 16:
+            self.lose_game_check()
+            self.game_lines()
+
+            if self.rows == self.rows_2 and self.colmuns == self.colmuns_2:
+                self.lose_game_screen()
+        
+        else:
+            return
+
+
+
+    def lose_game_check(self):
             for n in range(len(self.rows_2)):
                 row= self.rows_2[n]
-                colmun= self.colmuns_2[n]
 
                 #left check
                 x=0
@@ -582,14 +592,10 @@ class Game(QMainWindow):
                         
                 #To understand it get a sheet of paper and write it out
                 for j in range(len(row)):
-                    if row[j]:
-                        try:
-                            if row[j] == row[j-1]:
-                                row[j-1]= row[j] + row[j-1]
-                                row[j]=0
-
-                        except IndexError:
-                            pass
+                    if row[j] and j > 0:
+                        if row[j] == row[j-1]:
+                            row[j-1]= row[j] + row[j-1]
+                            row[j]=0
 
                 y=0
                 for i in range(len(row)):
@@ -597,77 +603,69 @@ class Game(QMainWindow):
                         #Use line below to switch values
                         row[y], row[i] = row[i], row[y]
                         y+=1
-                
+            
 
                 #right check
-                z=len(row) -1
-
+                x= len(row)-1
                 for i in range(len(row)-1, -1, -1):
                     if row[i]:
                         #Use line below to switch values
-                        row[z], row[i] = row[i], row[z]
-                        z-=1
+                        row[x], row[i] = row[i], row[x]
+                        x-=1
 
                 #To figure out write it out
                 for j in range(len(row)-1, -1, -1):
-                    if row[j]:
-                        try:
-                            if row[j] == row[j+1]:
-                                row[j]= row[j] + row[j+1]
-                                row[j+1]=0
-
-                        except IndexError:
-                            pass
+                    if row[j] and j+1 < len(row):
+                        if row[j] == row[j+1]:
+                            row[j]= row[j] + row[j+1]
+                            row[j+1]=0
 
 
-                A=len(row) -1
+                y=len(row) -1
                 for i in range(len(row)-1, -1, -1):
                     if row[i]:
                         #Use line below to switch values
-                        row[A], row[i] = row[i], row[A]
+                        row[y], row[i] = row[i], row[y]
                         y-=1
 
-
+            for n in range(len(self.colmuns_2)):
+                colmun= self.colmuns_2[n]
                 #up check
-                b=0
+                x=0
                 for i in range(len(colmun)):
                     if colmun[i]:
                         #Use line below to switch values
-                        colmun[b], colmun[i] = colmun[i], colmun[b]
-                        b+=1
+                        colmun[x], colmun[i] = colmun[i], colmun[x]
+                        x+=1
                         
 
                 for j in range(len(colmun)):
-                    if colmun[j]:
-                        try:
+                    if colmun[j] and j > 0:
                             if colmun[j] == colmun[j-1]:
                                 colmun[j-1]= colmun[j] + colmun[j-1]
                                 colmun[j]=0
 
-                        except IndexError:
-                            pass
 
-
-
-                c=0
+                y=0
                 for i in range(len(colmun)):
                     if colmun[i]:
                         #Use line below to switch values
-                        colmun[c], colmun[i] = colmun[i], colmun[c]
-                        c+=1
+                        colmun[y], colmun[i] = colmun[i], colmun[y]
+                        y+=1
+            
 
 
                 #down check
-                D=len(colmun) -1
+                x=len(colmun) -1
 
                 for i in range(len(colmun)-1, -1, -1):
                     if colmun[i]:
                         #Use line below to switch values
-                        colmun[D], colmun[i] = colmun[i], colmun[D]
+                        colmun[x], colmun[i] = colmun[i], colmun[x]
                         x-=1
 
                 for j in range(len(colmun)-1, -1, -1):
-                    if colmun[j]:
+                    if colmun[j] and j+1 < len(colmun):
                         try:
                             if colmun[j] == colmun[j+1]:
                                 colmun[j+1]= colmun[j] + colmun[j+1]
@@ -676,23 +674,56 @@ class Game(QMainWindow):
                         except IndexError:
                             pass
 
-                E=len(colmun) -1
+                y=len(colmun) -1
                 for i in range(len(colmun)-1, -1, -1):
                     if colmun[i]:
                         #Use line below to switch values
-                        colmun[E], colmun[i] = colmun[i], colmun[E]
-
+                        colmun[y], colmun[i] = colmun[i], colmun[y]
                         y-=1
 
-                print(self.rows_2)
 
-            #TODO fix bug
+    def lose_game_screen(self):
+        for i in range(len(self.boxes)):
+            self.boxes[i].hide()
 
-            if self.rows == self.rows_2 and self.colmuns == self.colmuns_2:
-                self.close()
+        lose_msg= QLabel('You Lost!')
+        lose_msg.setFixedSize(QSize(300, 100))
+        lose_msg.setStyleSheet('''
+            QLabel {
+            background: #FFFFFF;
+            color: #000000;
+            font: 40px;
+            border: 5px solid #000000;
+            }
+''')
+        lose_msg.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
+        leave= QPushButton('Back To Homescreen')
+        leave.setFixedSize(QSize(200, 100))
+        leave.setStyleSheet('''
+            QPushButton {
+                background: #696969;
+                color: #000000;
+                border-radius: 4px;
+                border: 1px solid #000000;
+                font: 20px;
+            }
 
+            QPushButton:hover {
+                background: #B0B0B0;
+            }
+
+''')
+        leave.clicked.connect(self.close)
         
+        button_layout= QHBoxLayout()
+
+        button_layout.addSpacing(4)
+        button_layout.addWidget(leave)
+
+
+        self.Game_layout.addWidget(lose_msg, 1, 1)
+        self.Game_layout.addLayout(button_layout, 2, 1)
 
 if __name__ == '__main__':
     main()
